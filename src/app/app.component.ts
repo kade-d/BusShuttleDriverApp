89866f88df79e85a1194f9667ec8396dc3c2438a
Keith DeSimini <kdesimini@bsu.edu>
2019-01-17 17:21:21 -0500

    Refactor and Organize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Log } from './log';
 import { LogService } from './log.service';
@@ -8,83 +8,49 @@ import { LogService } from './log.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  logs: Log[];
+export class AppComponent {
+  logs: Log;
   error = '';
   success = '';
   total = 0;
 
-  log = new Log(0, '', '', '', '', '');
+  log = new Log(0, '', '', '');
 
   constructor(private logService: LogService) {
   }
 
-  ngOnInit() {
-    this.getCars();
+  addLog(f): void {
+    this.logService.store(this.log).subscribe(
+      (data: Log) => {
+        console.log(data);
     
-  }
-
-  calculateTotal(): void {
-    this.total = 0;
-    for(let i = 0; i < this.logs.length; i++){
-      this.total = this.total + Number(this.logs[i].boarded);
-      console.log(this.total);
-    }
-  }
-  getCars(): void {
-    this.logService.getAll().subscribe(
-      (res: Log[]) => {
-        this.logs = res;
-        this.calculateTotal()
+        f.controls['boarded'].reset();
+        f.controls['stop'].reset();
+        f.controls['loop'].reset();
       },
-      (err) => {
-        this.error = err;
-      }
+      (error: any) => console.log("Could not add bitch")
+      
     );
   }
 
-  getGreen(): void {
-    this.logService.getAllGreen().subscribe(
-      (res: Log[]) => {
-        this.logs = res;
-        this.calculateTotal()
-      },
-      (err) => {
-        this.error = err;
-      }
-    );
-  }
+  // addCar(log: Log) {
+  //   this.resetErrors();
 
-  getRed(): void {
-    this.logService.getAllRed().subscribe(
-      (res: Log[]) => {
-        this.logs = res;
-        this.calculateTotal()
-      },
-      (err) => {
-        this.error = err;
-      }
-    );
-  }
+  //   this.logService.store(this.log)
+  //     .subscribe(
+  //       (res: Log) => {
+  //         // Update the list of cars
+  //         this.logs = res;
 
-  addCar(f) {
-    this.resetErrors();
+  //         // Inform the user
+  //         this.success = 'Created successfully';
 
-    this.logService.store(this.log)
-      .subscribe(
-        (res: Log[]) => {
-          // Update the list of cars
-          this.logs = res;
-
-          // Inform the user
-          this.success = 'Created successfully';
-
-          // Reset the form
-          f.reset();
-        },
-        (err) => this.error = err
-      );
-  }
+  //         // Reset the form
+  //         f.reset();
+  //       },
+  //       (err) => this.error = err
+  //     );
+  // }
 
   private resetErrors(){
     this.success = '';
--- src/app/log.ts
+++ src/app/log.ts
@@ -1,16 +1,12 @@
 export class Log {
   boarded: number;
   stop: string;
-  timestamp: string;
-  date: string;
   loop: string;
   driver: string;
   id?:   number;
   constructor(
     boarded: number,
     stop: string,
-    timestamp: string,
-    date: string,
     loop: string,
     driver: string,
     id?:   number) {}

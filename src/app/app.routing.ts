3c952437886c6a218b59962266515539e8f3316e
Keith DeSimini <kdesimini@bsu.edu>
2019-02-19 05:59:30 -0500

    Add confirmation component route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ConfigureComponent } from './configure/configure.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './Guards/auth.guard';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 const appRoutes: Routes = [
     {
@@ -19,6 +20,11 @@ const appRoutes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'confirmation',
+        component: ConfirmationComponent,
+        canActivate: [AuthGuard]
     },
 
     // otherwise redirect to home
